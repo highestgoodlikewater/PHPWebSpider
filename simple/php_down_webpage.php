519ed397e84c6f8373f05c8354be659152e8c126
bruceyoo <bruceyoo@sina.com>
2014-11-15 19:07:12 +0800

    Revert "Revert "phpwebspider first commit""
    This reverts commit d073caeae83d158c3ade3de234064cb5f661811e.

---
+++ simple/php_down_webpage.php
@@ -0,0 +1,29 @@
+<?php
+/*
+########################################################################     
+通过 php fopen 和 file 对网页进行打印
+fopen ：通过句柄单行获取网页，需要对网页句柄进行手动关闭；
+file ： 将网页文件一次下载到数组中，无需手动关闭；
+########################################################################                                        
+*/
+	
+	$target = "http://webbotsspidersscreenscrapers.com/hello_world.html";
+	// $target = "http://www.pansky.com.cn/";
+	
+	//fopen =========》》
+	/*
+	$file_handle = fopen($target,"r");
+	while(!N($file_handle)){
+		echo fgets($file_handle,4096);
+	}
+	fclose($file_handle);
+	*/
+	
+	//file ==========》》
+	/*
+	$downloaded_page_array=file($target);
+	for($i=0;$i<count($downloaded_page_array);$i++){
+		echo $downloaded_page_array[$i];
+	}
+	*/
+?>
